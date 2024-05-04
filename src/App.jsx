c1d48d1b49a6795ada5a8eb2afc29fd723aae89e
Thiemoko27 <traoredjobengs22@gmail.com>
2024-05-04 03:53:09 +0200

    Toujours en correction 1:52am

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,17 @@ import papierPeint from './assets/papier-peint.jpg';
 import { Card } from "./Components/card";
 
 function App() {
-  const bgStyle = {backgroundImage: `url(${papierPeint})`, backgroudSize: 'cover'}
+  const bgStyle = {backgroundImage: `url(${papierPeint})`, backgroudSize: 'cover', backgroudRepeat: 'no-repeat'}
 
   useEffect(() => {
     document.body.style.backgroundImage = bgStyle.backgroundImage
     document.body.style.backgroundSize = bgStyle.backgroudSize
+    document.body.style.backgroundRepeat = bgStyle.backgroudRepeat
 
     return(() => {
       document.body.style.backgroundImage =''
       document.body.style.backgroundSize =''
+      document.body.style.backgroundRepeat =''
     })
   }, [])
 
