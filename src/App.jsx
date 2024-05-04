5823809177dbe8c6ca386414b44c425ef2eeb090
Thiemoko27 <traoredjobengs22@gmail.com>
2024-05-04 04:21:46 +0200

    At last, 2:22am

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useFetch } from "./Components/Hooks/useFetch"
 import { Counter } from "./Components/Page/Counter"
-import papierPeint from './assets/papier-peint.jpg';
+import papierPeint from './assets/papier-peint.png';
 import { Card } from "./Components/card";
 
 function App() {
@@ -10,12 +10,10 @@ function App() {
   useEffect(() => {
     document.body.style.backgroundImage = bgStyle.backgroundImage
     document.body.style.backgroundSize = bgStyle.backgroudSize
-    document.body.style.backgroundRepeat = bgStyle.backgroudRepeat
 
     return(() => {
       document.body.style.backgroundImage =''
       document.body.style.backgroundSize =''
-      document.body.style.backgroundRepeat =''
     })
   }, [])
 
